ea6d6d40621be8b30f0089b7ee50b570db200ac2
Malacite <jared.ashley.thomas@gmail.com>
2020-08-22 17:30:59 -0400

    Basic level loading and menu implemented
    Added MainMenu with components for a name and a button. Added GameRoot to control game state, set GameRoot to be on load. Set up function to load MainMenu on init, then load Level1 on button press.
    
    Previous scene (Mainmenu) not removed on button press, need to fix.
    
    Also modified project to use 321 x 180 resolution

---
+++ GameRoot.gd
@@ -0,0 +1,63 @@
+extends Node
+
+# first %s - Levels or Menu
+# second %s - Level or Menu Name
+# third %s - Level or Menu number
+# const LVL_PATH = "res://Scenes/%s/%s%s.tscn" 
+
+var mainmenu
+
+
+var lvl_name:String = "Level1"
+
+func _ready():
+	call_deferred("init")
+	
+func init():
+	#If there are no children to the GameRoot node, then this is the intro scene, load main menu.
+	if(get_tree().root.get_child(0).get_children() == []):
+		load_level("MainMenu" ,"MainMenu")
+		#connect game initialization to button press
+		#todo: remove hardcode of mainmenu, as it's likely to break
+		mainmenu = get_tree().root.get_child(1)
+		print(mainmenu.connect("init_game_start", self, "on_init_game"))
+	#Otherwise, load level (useful for testing)
+	else:
+		load_level("Level1", "Level1")
+	
+func load_level(path:String ,name:String):
+	#example level name - OVERWORLD_main.tscn
+	#todo:check if the level to be loaded actually exists
+	var root = get_tree().root
+	
+	#check to see if a level is already loaded. If so, remove it first
+	#TODO - determine what to do with levels already loaded
+	if root.has_node("Level"):    #might need to fix
+		root.remove_child($Level)
+	elif root.has_node("Menu"):
+		root.remove_child($Control)
+	
+	var lvl_path_full
+	#TODO - add code to select specific level
+	#lvl_path_full = LVL_PATH %
+	
+	#temp
+	if(name == "MainMenu"):
+		lvl_path_full = "res://Scenes/Menus/MainMenu.tscn"
+	elif(name == "Level1"):
+		lvl_path_full = "res://Scenes/Levels/Level1.tscn"
+	elif(name == "Test"):
+		lvl_path_full = "res://Scenes/Test.tscn"
+	else:
+		return
+	
+	print(lvl_path_full)	
+	
+	var lvl = load(lvl_path_full).instance()
+	root.add_child(lvl)
+	return true
+	
+#ToDo - Alter function to take into account load states
+func on_init_game():
+	print ("signal to start game received")
+	load_level("Level1","Level1")
---
+++ Scenes/Menus/MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+signal init_game_start
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	print("Play game button pressed")
+	emit_signal("init_game_start")
