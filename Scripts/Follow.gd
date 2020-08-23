db9f1fc8b65b0c456b2aa9146ebf9729be47c9c8
Kelsala <aagrimhoysal@gmail.com>
2020-08-23 11:38:33 -0700

    Follow node implemented

---
+++ Scripts/Follow.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+export(NodePath) var node_to_follow
+export(Vector2) var offset
+const speed = 100
+onready var follow_node = get_node(node_to_follow)
+onready var parent = self.get_parent()
+
+func _physics_process(delta):
+	parent.position = parent.position + offset + (follow_node.position - parent.position).normalized() * delta * speed
