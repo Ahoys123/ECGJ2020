2c1a3773b22cd2627c89188f80a8ebdf112edb99
Malacite <jared.ashley.thomas@gmail.com>
2020-08-22 22:03:21 -0400

    Level Loading implemented
    Able to load levels based on level names. Can hook into the load level mechanic by connecting to a specific signal in GameRoot
    
    Will need to create a means of registering new signals upon level load.

--- Scripts/GameRoot.gd
+++ Scripts/GameRoot.gd
@@ -16,16 +16,16 @@ func _ready():
 func init():
 	#If there are no children to the GameRoot node, then this is the intro scene, load main menu.
 	if(get_tree().root.get_child(0).get_children() == []):
-		load_level("MainMenu" ,"MainMenu")
+		load_level("Menu" ,"MainMenu")
 		#connect game initialization to button press
 		#todo: remove hardcode of mainmenu, as it's likely to break
 		mainmenu = get_tree().root.get_child(1)
 		print(mainmenu.connect("init_game_start", self, "on_init_game"))
 	#Otherwise, load level (useful for testing)
 	else:
-		load_level("Level1", "Level1")
+		load_level("Level", "Level1")
 	
-func load_level(path:String ,name:String):
+func load_level(type:String ,name:String):
 	#example level name - OVERWORLD_main.tscn
 	#todo:check if the level to be loaded actually exists
 	var root = get_tree().root
@@ -33,19 +33,21 @@ func load_level(path:String ,name:String):
 	#check to see if a level is already loaded. If so, remove it first
 	#TODO - determine what to do with levels already loaded
 	if root.has_node("Level"):    #might need to fix
-		root.remove_child($Level)
-	elif root.has_node("Menu"):
-		root.remove_child($Control)
+		var level=root.get_node("Level")
+		root.remove_child(level)
+	if root.has_node("MainMenu"):
+		var level=root.get_node("MainMenu")
+		root.remove_child(level)
 	
 	var lvl_path_full
 	#TODO - add code to select specific level
 	#lvl_path_full = LVL_PATH %
 	
 	#temp
-	if(name == "MainMenu"):
-		lvl_path_full = "res://Scenes/Menus/MainMenu.tscn"
-	elif(name == "Level1"):
-		lvl_path_full = "res://Scenes/Levels/Level1.tscn"
+	if(type == "Menu"):
+		lvl_path_full = "res://Scenes/Menus/%s.tscn" % [name]
+	elif(type == "Level"):
+		lvl_path_full = "res://Scenes/Levels/%s.tscn" % [name]
 	elif(name == "Test"):
 		lvl_path_full = "res://Scenes/Test.tscn"
 	else:
@@ -60,4 +62,4 @@ func load_level(path:String ,name:String):
 #ToDo - Alter function to take into account load states
 func on_init_game():
 	print ("signal to start game received")
-	load_level("Level1","Level1")
+	load_level("Level","Level1")
